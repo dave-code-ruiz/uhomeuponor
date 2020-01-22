2cace903cbab3eaa51a819faeeaea89bf179541f
Dave Code <34915602+fcastroruiz@users.noreply.github.com>
2020-01-22 11:10:50 +0100

    Solved #1 reduce drastically number of API Calls

--- custom_components/uhomeuponor/__init__.py
+++ custom_components/uhomeuponor/__init__.py
@@ -11,12 +11,12 @@ UHOME_MODULE_KEYS = {
     'holiday_mode':                    {'addr': 22, 'value': 0},
     'forced_eco_mode':                 {'addr': 23, 'value': 0},
     'hc_mode':                         {'addr': 24, 'value': 0},
-#    'hc_masterslave':                  {'addr': 25, 'value': 0},
-#    'ts_sv_version':                   {'addr': 26, 'value': 0},
+    'hc_masterslave':                  {'addr': 25, 'value': 0},
+    'ts_sv_version':                   {'addr': 26, 'value': 0},
     'holiday_setpoint':                {'addr': 27, 'value': 0},
-#    'average_temp_low':                {'addr': 28, 'value': 0},
-#    'low_temp_alarm_limit':            {'addr': 29, 'value': 0},
-#    'low_temp_alarm_hysteresis':       {'addr': 30, 'value': 0},
+    'average_temp_low':                {'addr': 28, 'value': 0},
+    'low_temp_alarm_limit':            {'addr': 29, 'value': 0},
+    'low_temp_alarm_hysteresis':       {'addr': 30, 'value': 0},
     'remote_access_alarm':             {'addr': 31, 'value': 0},
     'device_lost_alarm':               {'addr': 32, 'value': 0},
     'no_comm_controller1':             {'addr': 33, 'value': 0},
@@ -24,9 +24,9 @@ UHOME_MODULE_KEYS = {
     'no_comm_controller3':             {'addr': 35, 'value': 0},
     'no_comm_controller4':             {'addr': 36, 'value': 0},
     'average_room_temperature':        {'addr': 37, 'value': 0},
-#    'controller_presence':             {'addr': 38, 'value': 0},
+    'controller_presence':             {'addr': 38, 'value': 0},
     'allow_hc_mode_change':            {'addr': 39, 'value': 0},
-#    'hc_master_type':                  {'addr': 40, 'value': 0},
+    'hc_master_type':                  {'addr': 40, 'value': 0},
 }
 
 
@@ -75,16 +75,21 @@ class Uhome(object):
         """
 
         self.uhome_thermostats = []
+        data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": ['
         for uc in self.uhome_controllers:
             # up to 12 thermostats per controller
             for i in range(0, 12):
                 # using room_setpoint to determine if thermostat is present
-                data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": [{"id": "' + \
-                       str(UHOME_THERMOSTAT_KEYS['room_setpoint']['addr'] + (500 * uc.index) + (40 * i)) + \
-                       '", "properties": {"85": {}}}]}}'
-                response_data = self.do_rest_call(data)
+                if i != 0 and uc.index != 0:
+                    data = data + ','
+                data = data + '{"id": "' + str(UHOME_THERMOSTAT_KEYS['room_setpoint']['addr'] + (500 * uc.index) + (40 * i)) + '", "properties": {"85": {}}}'
+        data = data + ']}}'
+        response_data = self.do_rest_call(data)
+        for uc in self.uhome_controllers:
+            for i in range(0, 12):
                 try:
-                    rs = response_data['result']['objects'][0]['properties']['85']['value']
+                    ix = i+(uc.index*12)
+                    rs = response_data['result']['objects'][ix]['properties']['85']['value']
                     if 0 < rs < 50:
                         self.uhome_thermostats.append(UhomeThermostat(uc.index, i))
                 except:
@@ -100,19 +105,25 @@ class Uhome(object):
 
         self.uhome_controllers = []
         # up to 4 controllers are supported
+        data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": ['
         for i in range(0, 4):
             # using software version to determine if a controller is present
-            data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": [{"id": "' + \
-                   str(UHOME_CONTROLLER_KEYS['controller_sv_version']['addr']+(500 * i)) + \
-                   '", "properties": {"85": {}}}]}}'
-            response_data = self.do_rest_call(data)
+            if i != 0:
+                data = data + ','
+            data = data + '{"id": "' + str(UHOME_CONTROLLER_KEYS['controller_sv_version']['addr']+(500 * i)) + '", "properties": {"85": {}}}'
+        data = data + ']}}'
+
+        response_data = self.do_rest_call(data)
+
+        for i in range(0, 4):
             try:
-                sw = response_data['result']['objects'][0]['properties']['85']['value']
+                sw = response_data['result']['objects'][i]['properties']['85']['value']
                 if sw != '0.00':
                     self.uhome_controllers.append(UhomeController(i))
             except:
                 break
                 pass
+
 
     def set_thermostat_value(self, thermostat, name, value):
         for key_name, key_data in thermostat.uhome_thermostat_keys.items():
@@ -161,13 +172,34 @@ class Uhome(object):
 
         return response_data
 
+#    def update_keys(self, keys):
+#        for ix, (key_name, key_data) in enumerate(keys.items()):
+#            data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": [{"id": "' + \
+#                   str(key_data['addr']) + '", "properties": {"85": {}}}]}}'
+#            response_data = self.do_rest_call(data)
+#            try:
+#                _LOGGER.info("data:" + str(ix))
+#                key_data['value'] = response_data['result']['objects'][0]['properties']['85']['value']
+#            except KeyError:
+#                pass
+#            except IndexError:
+#                pass
+#            except:
+#                break
+#                pass
+
     def update_keys(self, keys):
-        for key_name, key_data in keys.items():
-            data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": [{"id": "' + \
-                   str(key_data['addr']) + '", "properties": {"85": {}}}]}}'
-            response_data = self.do_rest_call(data)
+        data = '{"jsonrpc": "2.0", "id": 8, "method": "read", "params": {"objects": ['
+        for ix, (key_name, key_data) in enumerate(keys.items()):
+            if ix != 0:
+                data = data + ','
+            data = data + '{"id": "' + str(key_data['addr']) + '", "properties": {"85": {}}}'
+        data = data + ']}}'
+        response_data = self.do_rest_call(data)
+#        _LOGGER.info("data:" + str(data) + " response:" + str(response_data))
+        for ix, (key_name, key_data) in enumerate(keys.items()):
             try:
-                key_data['value'] = response_data['result']['objects'][0]['properties']['85']['value']
+                key_data['value'] = response_data['result']['objects'][ix]['properties']['85']['value']
             except KeyError:
                 pass
             except IndexError:
@@ -214,7 +246,7 @@ class UhomeController(object):
 
 UHOME_THERMOSTAT_KEYS = {
     'eco_profile_active_cf':           {'addr': 80, 'value': 0},
-#    'dehumidifier_control_activation': {'addr': 81, 'value': 0},
+    'dehumidifier_control_activation': {'addr': 81, 'value': 0},
     'rh_control_activation':           {'addr': 82, 'value': 0},
     'eco_profile_number':              {'addr': 83, 'value': 0},
     'setpoint_write_enable':           {'addr': 84, 'value': 0},
@@ -222,32 +254,32 @@ UHOME_THERMOSTAT_KEYS = {
     'rh_setpoint':                     {'addr': 86, 'value': 0},
     'min_setpoint':                    {'addr': 87, 'value': 0},
     'max_setpoint':                    {'addr': 88, 'value': 0},
-#    'min_floor_temp':                  {'addr': 89, 'value': 0},
-#    'max_floor_temp':                  {'addr': 90, 'value': 0},
+    'min_floor_temp':                  {'addr': 89, 'value': 0},
+    'max_floor_temp':                  {'addr': 90, 'value': 0},
     'room_setpoint':                   {'addr': 91, 'value': 0},
     'eco_offset':                      {'addr': 92, 'value': 0},
     'eco_profile_active':              {'addr': 93, 'value': 0},
     'home_away_mode_status':           {'addr': 94, 'value': 0},
     'room_in_demand':                  {'addr': 95, 'value': 0},
     'rh_limit_reached':                {'addr': 96, 'value': 0},
-#    'floor_limit_status':              {'addr': 97, 'value': 0},
+    'floor_limit_status':              {'addr': 97, 'value': 0},
     'technical_alarm':                 {'addr': 98, 'value': 0},
-#    'tamper_indication':               {'addr': 99, 'value': 0},
+    'tamper_indication':               {'addr': 99, 'value': 0},
     'rf_alarm':                        {'addr': 100, 'value': 0},
     'battery_alarm':                   {'addr': 101, 'value': 0},
     'rh_sensor':                       {'addr': 102, 'value': 0},
     'thermostat_type':                 {'addr': 103, 'value': 0},
     'regulation_mode':                 {'addr': 104, 'value': 0},
     'room_temperature':                {'addr': 105, 'value': 0},
-#    'room_temperature_ext':            {'addr': 106, 'value': 0},
+    'room_temperature_ext':            {'addr': 106, 'value': 0},
     'rh_value':                        {'addr': 107, 'value': 0},
-#    'ch_linked_to_th':                 {'addr': 108, 'value': 0},
+    'ch_linked_to_th':                 {'addr': 108, 'value': 0},
     'room_name':                       {'addr': 109, 'value': 0},
-#    'utilization_factor_24h':          {'addr': 110, 'value': 0},
-#    'utilization_factor_7d':           {'addr': 111, 'value': 0},
-#    'reg_mode':                        {'addr': 112, 'value': 0},
-#    'channel_average':                 {'addr': 113, 'value': 0},
-#    'radiator_heating':                {'addr': 114, 'value': 0}
+    'utilization_factor_24h':          {'addr': 110, 'value': 0},
+    'utilization_factor_7d':           {'addr': 111, 'value': 0},
+    'reg_mode':                        {'addr': 112, 'value': 0},
+    'channel_average':                 {'addr': 113, 'value': 0},
+    'radiator_heating':                {'addr': 114, 'value': 0}
 }
 
 
@@ -269,6 +301,7 @@ class UhomeThermostat(object):
         self.uhome_thermostat_keys = {}
         self.uc_index = uc_index
         self.index = index
+        self.identity = "controller" + str(uc_index) + "_thermostat" + str(index)
         for key_name, key_data in UHOME_THERMOSTAT_KEYS.items():
             # address in calculated with following: addr + (500 x controller_index) + (40 x thermostat_index)
             self.uhome_thermostat_keys[key_name] = {'addr': (key_data['addr'] + (500 * uc_index) + (40 * index)), 'value': 0}
