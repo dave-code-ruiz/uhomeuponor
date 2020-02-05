1aa26815b5062343a0bbf5f84c956a887584d85f
Michael Bisbjerg <michael@mbwarez.dk>
2020-02-06 00:11:08 +0100

    Update presets to Uponor naming, use IDLE instead of OFF

--- custom_components/uhomeuponor/climate.py
+++ custom_components/uhomeuponor/climate.py
@@ -13,8 +13,8 @@ from homeassistant.components.climate import PLATFORM_SCHEMA, ClimateDevice
 from homeassistant.components.climate.const import (
     DOMAIN, 
     HVAC_MODE_AUTO, HVAC_MODE_OFF, HVAC_MODE_HEAT, HVAC_MODE_COOL, 
-    PRESET_ECO, PRESET_COMFORT,
-    CURRENT_HVAC_HEAT, CURRENT_HVAC_OFF, CURRENT_HVAC_COOL, 
+    PRESET_HOME, PRESET_AWAY,
+    CURRENT_HVAC_HEAT, CURRENT_HVAC_COOL, CURRENT_HVAC_IDLE,
     SUPPORT_PRESET_MODE, SUPPORT_TARGET_TEMPERATURE)
 from homeassistant.const import (
     ATTR_ATTRIBUTION, ATTR_ENTITY_ID, ATTR_TEMPERATURE, ATTR_BATTERY_LEVEL, 
@@ -119,7 +119,7 @@ class UponorThermostat(ClimateDevice):
 
     @property
     def preset_modes(self):
-        return [PRESET_COMFORT, PRESET_ECO]
+        return [PRESET_HOME, PRESET_AWAY]
 
     # ** State **
     @property
@@ -136,45 +136,46 @@ class UponorThermostat(ClimateDevice):
     
     @property
     def device_state_attributes(self):
-        attrs = {
+        return {
             ATTR_TECHNICAL_ALARM: self.thermostat.by_name('technical_alarm').value,
             ATTR_RF_SIGNAL_ALARM: self.thermostat.by_name('rf_alarm').value,
             ATTR_BATTERY_ALARM: self.thermostat.by_name('battery_alarm').value
         }
-        return attrs
         
     @property
     def preset_mode(self):
         if self.thermostat.by_name('home_away_mode_status').value == 1:
-            return PRESET_ECO
-        else:
-            return PRESET_COMFORT
+            return PRESET_AWAY
+
+        return PRESET_HOME
 
     @property
     def hvac_mode(self):
         if self.uponor_client.uhome.by_name('hc_mode').value == 1:
             return HVAC_MODE_COOL
-        else:
-            return HVAC_MODE_HEAT
+
+        return HVAC_MODE_HEAT
 
     @property
     def hvac_action(self):
-        if self.thermostat.by_name('room_in_demand').value == 1:
-            if self.hvac_mode == HVAC_MODE_HEAT:
-                return CURRENT_HVAC_HEAT
-            else:
-                return CURRENT_HVAC_COOL
+        if self.thermostat.by_name('room_in_demand').value == 0:
+            return CURRENT_HVAC_IDLE
+        
+        if self.hvac_mode == HVAC_MODE_HEAT:
+            return CURRENT_HVAC_HEAT
         else:
-            return CURRENT_HVAC_OFF
+            return CURRENT_HVAC_COOL
 
     # ** Actions **
     def update(self):
         # Update Uhome (to get HC mode) and thermostat
         self.uponor_client.update_devices(self.uponor_client.uhome, self.thermostat)
 
+    # TODO: Support setting hvac_mode
     # def set_hvac_mode(self, hvac_mode):
     #     value = '0'
 
+    # TODO: Support setting preset_mode
     # def set_preset_mode(self, preset_mode):
     #     value = '0'
 
