6017fe503d23a0ea1dc44e85262d1525184c873e
Dave Code <34915602+dave-code-ruiz@users.noreply.github.com>
2021-04-15 13:37:30 +0200

    Update __init__.py

--- custom_components/uhomeuponor/__init__.py
+++ custom_components/uhomeuponor/__init__.py
@@ -3,3 +3,28 @@
 For more details about this platform, please refer to the documentation at
 https://github.com/fcastroruiz/uhomeuponor
 """
+from homeassistant.const import CONF_HOST
+from logging import getLogger
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+_LOGGER = getLogger(__name__)
+DOMAIN = "uhomeuponor"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up this integration using UI."""
+    _LOGGER.info("Loading setup")
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN]["config"] = config.get(DOMAIN) or {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+    """Set up this integration using UI."""
+    host = config_entry.data[CONF_HOST]
+    _LOGGER.info("Loading setup entry")
+
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(config_entry, "climate"))
+    hass.async_create_task(hass.config_entries.async_forward_entry_setup(config_entry, "sensor"))
+
+    return True
+
