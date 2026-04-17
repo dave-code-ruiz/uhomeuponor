95840d701fad416f404d9ca9438755c94b1a61af
dave-code-ruiz <davecoderuiz@gmail.com>
2026-04-17 11:30:13 +0200

    fix: remove OptionsFlowHandler.__init__ to avoid config_entry setter error in HA

--- custom_components/uhomeuponor/config_flow.py
+++ custom_components/uhomeuponor/config_flow.py
@@ -76,13 +76,9 @@ class UhomeuponorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(entry: config_entries.ConfigEntry):
-        return OptionsFlowHandler(entry)
+        return OptionsFlowHandler()
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        super().__init__()
 
     async def async_step_init(self, _user_input=None):
         """Manage the options."""
